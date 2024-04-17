2096d73081022751a8bd8502d3c7f2aa500cb7d1
Ajitesh1705 <ajiteshsaharan2@gmail.com>
2024-04-17 18:42:14 +0530

    new

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const cors=require('cors');
 require('dotenv').config();
 require('./config/db');
 const PORT = process.env.PORT || 8080;
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use('/api/v1', routes);
-app.use(cors())
 
 app.listen(PORT, ()=>{
     console.log(`Server is up and running on PORT: ${PORT}`);
